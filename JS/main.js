50ef2665d58a303145de25600a089db71777cca3
jeedd95 <hyungwoo10@naver.com>
2023-03-19 23:04:50 +0900

    시계 기능

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
+const clock = document.querySelector('#clock');
+const loginBtn = document.querySelector('#Login');
+const signinBtn = document.querySelector('#Signin');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.listen(5500)
+function getTime() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function Login() {
+  console.log("로그인 버튼 클릭");
+}
+
+function Signin() {
+  console.log("회원가입 클릭");
+}
+
+getTime();
+setInterval(getTime, 1000);
+loginBtn.addEventListener("click", Login);
+signinBtn.addEventListener("click",Signin);
