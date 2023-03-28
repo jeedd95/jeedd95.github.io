7e37ec76189dfb62b9c59810192eb0c8d6af451c
jeedd95 <jeedd95@gmail.com>
2023-03-29 00:59:06 +0900

    파이어베이스 연동

--- JS/Firebase/config.js
+++ JS/Firebase/config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { initializeApp } from "../../node_modules/firebase/firebase-app.js";
+//import { getAnalytics } from "../../node_modules/firebase/firebase-analytics.js";
+import { getAuth,createUserWithEmailAndPassword} from "../../node_modules/firebase/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
 createUserWithEmailAndPassword(auth, email, password)
@@ -33,5 +34,3 @@ createUserWithEmailAndPassword(auth, email, password)
     const errorMessage = error.message;
     // ..
   });
-
-  createUserWithEmailAndPassword(auth,"asdasd@naver.com","123123");
--- JS/signIn.js
+++ JS/signIn.js
@@ -3,28 +3,11 @@ const emailInput = document.querySelector("#signInForm .emailInput");
 const passwordInput = document.querySelectorAll("#signInForm .passwordInput");
 const signInButton = document.querySelector("#signInForm .signInButton");
 
-//import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-// const auth = getAuth();
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
-//   });
-
 function SignIn(event){
     event.preventDefault();
     console.log("가입버튼 클릭");
     console.log(emailInput.innerText);
     console.log(passwordInput.innerText);
-
-    //createUserWithEmailAndPassword(auth,emailInput.innerText,passwordInput.innerText);
 }
 
 signInButton.addEventListener("click",SignIn);
