6b09d940e6a6074cac2a036338a7aef67fb2b6dc
jeedd95 <jeedd95@gmail.com>
2023-04-03 02:14:55 +0900

    회원가입 firestore 저장, 중복 확인 비밀번호 일치하는지 확인

--- JS/Firebase/config.js
+++ JS/Firebase/config.js
@@ -1,9 +1,13 @@
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "../../node_modules/firebase/firebase-app.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "../../node_modules/firebase/firebase-auth.js";
+import {
+  getAuth, createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, onAuthStateChanged,
+  updateProfile
+} from "../../node_modules/firebase/firebase-auth.js";
+import { doc, setDoc, getDocs, getFirestore, collection, query, where, addDoc } from "../../node_modules/firebase/firebase-firestore.js";
 //import { getAnalytics } from "../../node_modules/firebase/firebase-analytics.js";
-//import { getFirestore } from "../../node_modules/firebase/firebase-firestore.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,16 +24,21 @@ const firebaseConfig = {
   measurementId: "G-FZL1XH7BC6"
 };
 
+/** 컬렉션 이름 */
+const DocRef = {
+  USERS: "users"
+}
+Object.freeze(DocRef);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-//const db = getFirestore(app);
+const db = getFirestore(app);
 //const analytics = getAnalytics(app);
 
 /** 회원가입 */
-export function CreateAccount(email, password, Success,Error) {
-
-  createUserWithEmailAndPassword(auth, email, password)
+export function CreateAccount(user, Success, Error) {
+  createUserWithEmailAndPassword(auth, user.email, user.password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
@@ -43,3 +52,59 @@ export function CreateAccount(email, password, Success,Error) {
       Error(errorCode);
     })
 }
+
+/** 로그인 */
+export function SignIn(email, password, Success, Error) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+      Success(user);
+
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Error(errorCode);
+    });
+}
+
+/** 
+ * FireStore 데이터 저장
+ * @param {object} myData 내 데이터
+ * @param {string} uid Auth에서 생성되는 uid
+ */
+export async function AddDocData(myData, uid) {
+  await setDoc(doc(db, DocRef.USERS, uid), myData);
+
+  // try {
+  //   const docRef = await addDoc(collection(db, "users",uid),myData);
+  //   console.log("Document written with ID: ", docRef.id);
+  // } catch (e) {
+  //   console.error("Error adding document: ", e);
+  // }
+}
+
+/**
+ * FireStroe 데이터 검색
+ * @param {string} ref 찾을 필드
+ * @param {*} input 검색 데이터
+ */
+export async function FindDocData(ref, input, callback) {
+  const q = query(collection(db, DocRef.USERS), where(ref, "==", input));
+
+  const querySnapshot = await getDocs(q);
+
+  if(querySnapshot.size > 0){
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+    callback(true);
+  }
+  else{
+    callback(false);
+  }
+  }
+
--- JS/signIn.js
+++ JS/signIn.js
@@ -1,4 +1,4 @@
-import { CreateAccount } from "./Firebase/config.js";
+import { AddDocData, CreateAccount, FindDocData } from "./Firebase/config.js";
 
 const signInForm = document.querySelector("#signInForm");
 
@@ -6,20 +6,61 @@ const inputs = signInForm.getElementsByTagName("input");
 const emailInput = inputs[0];
 const passwordInput = inputs[1];
 const passwordCheckInput = inputs[2];
+const nickNameInput = inputs[3];
 const buttons = signInForm.getElementsByTagName("button");
 const checkDuplicateButton = buttons[0];
 const signInButton = buttons[1];
+const infos = signInForm.getElementsByTagName("label");
+const passwordInfo=infos[0];
 
+let isCompletePassword;
+
+/** 가입 버튼 클릭 */
 function SignIn(event) {
     event.preventDefault();
-    CreateAccount(emailInput.value, passwordInput.value,
-        (success) => {
-            console.log("회원가입 성공");
+    const myData = {
+        email: emailInput.value,
+        password: passwordInput.value,
+        nickName: nickNameInput.value
+    }
+    CreateAccount(myData,
+        (user) => {
+            AddDocData(myData, user.uid);
+            alert(`[${myData.nickName}] 회원 가입 성공`);
         },
-        (error) => {
-            console.log("회원가입 실패");
+        (errorCode) => {
+            alert(`회원 가입 실패\n에러코드 : ${errorCode}`);
         })
 }
 
+/** 중복 확인 버튼 클릭 */
+function CheckDuplicateDB(event) {
+    event.preventDefault();
+    //이메일에 써놓은 정보를 users에서 검색해서 이미 있으면 중복됬다고 알린다
+    FindDocData("email", emailInput.value, (callback) => {
+        if (callback === true) {
+            alert("중복된 이메일입니다.");
+            emailInput.value = "";
+        }
+        else {
+            alert("사용 가능한 이메일입니다.");
+        }
+    });
+}
 
+/** 비밀번호 일치 확인 */
+function PasswordInput(){
+    if(passwordInput.value !== passwordCheckInput.value){
+        passwordInfo.innerText ="비밀번호가 일치 하지않습니다.";
+        isCompletePassword=false;
+    }
+    else{
+        passwordInfo.innerText="";
+        isCompletePassword=true;
+    }
+}
+
+checkDuplicateButton.addEventListener("click", CheckDuplicateDB);
 signInButton.addEventListener("click", SignIn);
+passwordInput.addEventListener("input",PasswordInput);
+passwordCheckInput.addEventListener("input",PasswordInput);
