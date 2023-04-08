71c43ef27f74686e0d689ce2ac90ad50a10590c3
jeedd95 <jeedd95@gmail.com>
2023-04-09 02:06:42 +0900

    모듈들 cdn형식 변경 이메일 정규식 가입 기능 완료

--- JS/Firebase/config.js
+++ JS/Firebase/config.js
@@ -1,14 +1,7 @@
 // Import the functions you need from the SDKs you need
-//import { initializeApp } from "../../node_modules/firebase/firebase-app.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
-import {
-  getAuth, createUserWithEmailAndPassword,
-  signInWithEmailAndPassword, onAuthStateChanged,
-  updateProfile
-} from "../../node_modules/firebase/firebase-auth.js";
-import { doc, setDoc, getDocs, getFirestore, collection, query, where, addDoc } from "../../node_modules/firebase/firebase-firestore.js";
-
-//import { getAnalytics } from "../../node_modules/firebase/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, updateProfile } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+import { doc, setDoc, getDocs, getFirestore, collection, query, where, addDoc } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,17 +18,15 @@ const firebaseConfig = {
   measurementId: "G-FZL1XH7BC6"
 };
 
-/** 컬렉션 이름 */
-const DocRef = {
+const DocRef = Object.freeze({
   USERS: "users"
-}
-Object.freeze(DocRef);
+});
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-//const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 /** 회원가입 */
 export function CreateAccount(user, Success, Error) {
@@ -77,10 +68,10 @@ export function SignIn(email, password, Success, Error) {
  * @param {string} uid Auth에서 생성되는 uid
  */
 export async function AddDocData(myData, uid) {
-  await setDoc(doc(db, DocRef.USERS, uid), myData);
+   await setDoc(doc(db, DocRef.USERS, uid), myData);
 
   // try {
-  //   const docRef = await addDoc(collection(db, "users",uid),myData);
+  //   const docRef = await addDoc(collection(db, DocRef.USERS),myData);
   //   console.log("Document written with ID: ", docRef.id);
   // } catch (e) {
   //   console.error("Error adding document: ", e);
@@ -97,15 +88,15 @@ export async function FindDocData(ref, input, callback) {
 
   const querySnapshot = await getDocs(q);
 
-  if(querySnapshot.size > 0){
+  if (querySnapshot.size > 0) {
     querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       console.log(doc.id, " => ", doc.data());
     });
     callback(true);
   }
-  else{
+  else {
     callback(false);
   }
-  }
+}
 
--- JS/signIn.js
+++ JS/signIn.js
@@ -1,5 +1,4 @@
 import { AddDocData, CreateAccount, FindDocData } from "./Firebase/config.js";
-import VerEx from '../node_modules/verbal-expressions/VerbalExpressions.js';
 
 const signInForm = document.querySelector("#signInForm");
 
@@ -12,13 +11,20 @@ const buttons = signInForm.getElementsByTagName("button");
 const checkDuplicateButton = buttons[0];
 const signInButton = buttons[1];
 const infos = signInForm.getElementsByTagName("label");
-const passwordInfo = infos[0];
+const emailInfo = infos[0];
+const passwordInfo = infos[1];
 
-let isCompletePassword;
+let isCompleteEmail=false;
+let isCompletePassword=false;
+//let isCompleteEmailDuplicate=false;
 
 /** 가입 버튼 클릭 */
 function SignIn(event) {
     event.preventDefault();
+    if(!isCompleteEmail){ alert("이메일을 확인해주세요"); return;}
+    if(!isCompletePassword){alert("비밀번호를 확인해주세요"); return;}
+    //if(!isCompleteEmailDuplicate){alert("이메일 중복을 확인해주세요"); return;}
+
     const myData = {
         email: emailInput.value,
         password: passwordInput.value,
@@ -34,18 +40,11 @@ function SignIn(event) {
         })
 }
 
-/** 중복 확인 버튼 클릭 */
-function CheckDuplicateDB(event) {
-    event.preventDefault();
-    //이메일에 써놓은 정보를 users에서 검색해서 이미 있으면 중복됬다고 알린다
-    FindDocData("email", emailInput.value, (callback) => {
-        if (callback === true) {
-            alert("중복된 이메일입니다.");
-            emailInput.value = "";
-        }
-        else {
-            alert("사용 가능한 이메일입니다.");
-        }
+/** 이메일 중복 확인 */
+function CheckDuplicateDB(email) {
+    return new Promise((resolve, reject) => {
+        //이메일에 써놓은 정보를 users에서 검색해서 이미 있으면 중복됬다고 알린다
+        FindDocData("email", email, (result) => { resolve(result); });
     });
 }
 
@@ -60,19 +59,44 @@ function PasswordInput() {
         isCompletePassword = true;
     }
 }
-/** 이메일 정규식 생성 */
-function GenerateRegex(email) {
+/** 이메일 정규식 검증 */
+function EmailRegex(email) {
     const emailRegex = VerEx()
         .startOfLine()
-        .something()
-        .maybe('@')
+        .then(/[a-zA-Z0-9._%+-]+/)
+        .then('@')
+        .then(/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)
         .endOfLine()
 
-    return emailRegex.test(email);
+        return emailRegex.test(email);
 }
+
+async function EmailCheck(event){
+    event.preventDefault();
+
+    let regex = EmailRegex(emailInput.value);
+    let db= await CheckDuplicateDB(emailInput.value);
+    
+    if (!regex) {
+        emailInfo.innerText = "이메일 형식을 확인해주세요";
+        emailInfo.className = "wrong"
+        isCompleteEmail=false;
+    }else if(db)
+    {
+        emailInfo.innerText = "중복된 이메일 입니다.";
+        emailInfo.className = "wrong"
+        isCompleteEmail=false;
+    }
+    else{
+        emailInfo.innerText = "사용 가능한 이메일입니다.";
+        emailInfo.className = "correct"
+        isCompleteEmail=true;
+    }
+}
+
 
 checkDuplicateButton.addEventListener("click", CheckDuplicateDB);
 signInButton.addEventListener("click", SignIn);
+emailInput.addEventListener("blur",EmailCheck);
 passwordInput.addEventListener("input", PasswordInput);
 passwordCheckInput.addEventListener("input", PasswordInput);
-console.log(GenerateRegex("ads@naver.com"));
