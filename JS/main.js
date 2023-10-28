7a78e159aea13ab6994e4c51111c2812317caf92
JHW <jeedd95@gmail.com>
2023-10-28 23:10:05 +0900

    상대 경로 수정
    Signed-off-by: JHW <jeedd95@gmail.com>

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,12 @@
 import { SignIn, LogOut, SignOut } from "./Firebase/config.js";
 
-const clock = document.querySelector('#clock');
-const loginBtn = document.querySelector('#Login');
-const signinBtn = document.querySelector('#Signin');
-const LogOutBtn = document.querySelector('#Logout');
-const emailInput = document.querySelector('#emailInput');
-const passwordInput = document.querySelector('#passwordInput');
-const signOutBtn = document.querySelector('#Signout');
-
-
+const clock = document.querySelector("#clock");
+const loginBtn = document.querySelector("#Login");
+const signinBtn = document.querySelector("#Signin");
+const LogOutBtn = document.querySelector("#Logout");
+const emailInput = document.querySelector("#emailInput");
+const passwordInput = document.querySelector("#passwordInput");
+const signOutBtn = document.querySelector("#Signout");
 
 function getTime() {
   const date = new Date();
@@ -21,33 +19,38 @@ function getTime() {
 
 function Login() {
   console.log("로그인 버튼 클릭");
-  SignIn(emailInput.value, passwordInput.value, (Success) => {
-    alert("로그인에 성공하셨습니다!");
-    window.location.assign('menu.html');
-  },
+  SignIn(
+    emailInput.value,
+    passwordInput.value,
+    (Success) => {
+      alert("로그인에 성공하셨습니다!");
+      window.location.assign("HTML/menu.html");
+    },
     (Error) => {
       alert(`로그인에 실패하셨습니다!\n${Error}`);
-    });
+    }
+  );
 }
 
 function Signin() {
   console.log("회원가입 클릭");
-  window.location.assign('auth.html');
+  window.location.assign("HTML/auth.html");
 }
 
 function _SignOut() {
   console.log("회원탈퇴 클릭");
-  SignOut((user) => {
-    //DB삭제
-    //DeleteDB(user.uid);
-    alert(`탈퇴 되었습니다!`);
-    //console.log(user);
-  }, (Error) => {
-    alert(`탈퇴 실패...`);
-    //console.log(Error);
-  });
-
-  
+  SignOut(
+    (user) => {
+      //DB삭제
+      //DeleteDB(user.uid);
+      alert(`탈퇴 되었습니다!`);
+      //console.log(user);
+    },
+    (Error) => {
+      alert(`탈퇴 실패...`);
+      //console.log(Error);
+    }
+  );
 }
 
 getTime();
--- JS/signIn.js
+++ JS/signIn.js
@@ -14,90 +14,97 @@ const infos = signInForm.getElementsByTagName("label");
 const emailInfo = infos[0];
 const passwordInfo = infos[1];
 
-let isCompleteEmail=false;
-let isCompletePassword=false;
+let isCompleteEmail = false;
+let isCompletePassword = false;
 //let isCompleteEmailDuplicate=false;
 
 /** 가입 버튼 클릭 */
 function SignIn(event) {
-    event.preventDefault();
-    if(!isCompleteEmail){ alert("이메일을 확인해주세요"); return;}
-    if(!isCompletePassword){alert("비밀번호를 확인해주세요"); return;}
-    //if(!isCompleteEmailDuplicate){alert("이메일 중복을 확인해주세요"); return;}
+  event.preventDefault();
+  if (!isCompleteEmail) {
+    alert("이메일을 확인해주세요");
+    return;
+  }
+  if (!isCompletePassword) {
+    alert("비밀번호를 확인해주세요");
+    return;
+  }
+  //if(!isCompleteEmailDuplicate){alert("이메일 중복을 확인해주세요"); return;}
 
-    const myData = {
-        email: emailInput.value,
-        password: passwordInput.value,
-        nickName: nickNameInput.value
+  const myData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+    nickName: nickNameInput.value,
+  };
+  CreateAccount(
+    myData,
+    (user) => {
+      AddDocData(myData, user.uid, () => {
+        window.location.assign("index.html");
+      });
+      alert(`[${myData.nickName}] 회원 가입 성공`);
+    },
+    (errorCode) => {
+      alert(`회원 가입 실패\n에러코드 : ${errorCode}`);
     }
-    CreateAccount(myData,
-        (user) => {
-            AddDocData(myData, user.uid,()=>{window.location.assign('index.html');
-        });
-            alert(`[${myData.nickName}] 회원 가입 성공`);
-        },
-        (errorCode) => {
-            alert(`회원 가입 실패\n에러코드 : ${errorCode}`);
-        })
+  );
 }
 
 /** 이메일 중복 확인 */
 function CheckDuplicateDB(email) {
-    return new Promise((resolve, reject) => {
-        //이메일에 써놓은 정보를 users에서 검색해서 이미 있으면 중복됬다고 알린다
-        FindDocData("email", email, (result) => { resolve(result); });
+  return new Promise((resolve, reject) => {
+    //이메일에 써놓은 정보를 users에서 검색해서 이미 있으면 중복됬다고 알린다
+    FindDocData("email", email, (result) => {
+      resolve(result);
     });
+  });
 }
 
 /** 비밀번호 일치 확인 */
 function PasswordInput() {
-    if (passwordInput.value !== passwordCheckInput.value) {
-        passwordInfo.innerText = "비밀번호가 일치 하지않습니다.";
-        isCompletePassword = false;
-    }
-    else {
-        passwordInfo.innerText = "";
-        isCompletePassword = true;
-    }
+  if (passwordInput.value !== passwordCheckInput.value) {
+    passwordInfo.innerText = "비밀번호가 일치 하지않습니다.";
+    isCompletePassword = false;
+  } else {
+    passwordInfo.innerText = "";
+    isCompletePassword = true;
+  }
 }
 /** 이메일 정규식 검증 */
 function EmailRegex(email) {
-    const emailRegex = VerEx()
-        .startOfLine()
-        .then(/[a-zA-Z0-9._%+-]+/)
-        .then('@')
-        .then(/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)
-        .endOfLine()
+  const emailRegex = VerEx()
+    .startOfLine()
+    .then(/[a-zA-Z0-9._%+-]+/)
+    .then("@")
+    .then(/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)
+    .endOfLine();
 
-        return emailRegex.test(email);
+  return emailRegex.test(email);
 }
 
-async function EmailCheck(event){
-    event.preventDefault();
+async function EmailCheck(event) {
+  event.preventDefault();
 
-    let regex = EmailRegex(emailInput.value);
-    let db= await CheckDuplicateDB(emailInput.value);
-    
-    if (!regex) {
-        emailInfo.innerText = "이메일 형식을 확인해주세요";
-        emailInfo.className = "wrong"
-        isCompleteEmail=false;
-    }else if(db)
-    {
-        emailInfo.innerText = "중복된 이메일 입니다.";
-        emailInfo.className = "wrong"
-        isCompleteEmail=false;
-    }
-    else{
-        emailInfo.innerText = "사용 가능한 이메일입니다.";
-        emailInfo.className = "correct"
-        isCompleteEmail=true;
-    }
+  let regex = EmailRegex(emailInput.value);
+  let db = await CheckDuplicateDB(emailInput.value);
+
+  if (!regex) {
+    emailInfo.innerText = "이메일 형식을 확인해주세요";
+    emailInfo.className = "wrong";
+    isCompleteEmail = false;
+  } else if (db) {
+    emailInfo.innerText = "중복된 이메일 입니다.";
+    emailInfo.className = "wrong";
+    isCompleteEmail = false;
+  } else {
+    emailInfo.innerText = "사용 가능한 이메일입니다.";
+    emailInfo.className = "correct";
+    isCompleteEmail = true;
+  }
 }
-
 
 checkDuplicateButton.addEventListener("click", CheckDuplicateDB);
 signInButton.addEventListener("click", SignIn);
-emailInput.addEventListener("blur",EmailCheck);
+emailInput.addEventListener("blur", EmailCheck);
 passwordInput.addEventListener("input", PasswordInput);
 passwordCheckInput.addEventListener("input", PasswordInput);
